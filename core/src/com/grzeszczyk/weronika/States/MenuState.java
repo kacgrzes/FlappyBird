c383189294b7757c399f9752335868a5b0e4423d
Kacper Grzeszczyk <kacgrzes@gmail.com>
2018-05-28 19:51:25 +0200

    Initialize repository

---
+++ core/src/com/grzeszczyk/weronika/States/MenuState.java
@@ -0,0 +1,30 @@
+package com.grzeszczyk.weronika.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import javax.xml.soap.Text;
+
+/**
+ * Created on 28.05.2018.
+ */
+
+public class MenuState extends State{
+    public MenuState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+}
